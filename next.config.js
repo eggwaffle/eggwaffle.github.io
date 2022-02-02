93fa70a2c468ffb67978202eed1e5aaf4fa0ae58
eggwaffle <29174802+eggwaffle@users.noreply.github.com>
2022-02-03 10:28:00 +1030

    Add image loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-/*  images: {
+  images: {
     loader: 'imgix',
     path: 'https://eggwaffle.github.io',
-  },*/
+  },
 }
 
 module.exports = nextConfig
