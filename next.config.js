d276c7f1a90c73099f1ac926c1fed71af8adff9f
eggwaffle <29174802+eggwaffle@users.noreply.github.com>
2022-03-06 18:14:43 +1030

    Stand with Ukraine

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['eggwaffle.github.io', 'images.unsplash.com', 'raw.githubusercontent.com','img.shields.io'],
-  //  loader: 'imgix',
-  //  path: '',
+  //  domains: ['eggwaffle.github.io', 'images.unsplash.com', 'raw.githubusercontent.com','img.shields.io'],
+    loader: 'imgix',
+    path: '',
   },
 }
 
